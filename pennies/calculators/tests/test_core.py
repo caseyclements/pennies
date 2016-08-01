3ccc9d65ba3614b4e3f058cd91f2d121e6bff33e
Richard Postelnik <rpostelnik@continuum.io>
2016-07-31 22:11:49 -0400

    parametrize pv test

--- pennies/calculators/tests/test_core.py
+++ pennies/calculators/tests/test_core.py
@@ -5,17 +5,14 @@ from pennies.assets.core import CashFlow
 from pennies.market.curves import ConstantDiscountRateCurve
 from pennies.market.market import RatesTermStructure
 
-
-def test_cash_flow_pv():
-    today = datetime(2015, 1, 1)
-    curve = ConstantDiscountRateCurve(today, 0.0)
+@pytest.mark.parametrize("today,cf_date,rate,expected", [
+    (datetime(2015, 1, 1), datetime(2016, 1, 1), 0.0, 100.0),
+    (datetime(2015, 1, 1), datetime(2016, 1, 1), 0.01, 99.005),
+    (datetime(2015, 1, 1), datetime(2014, 12, 31), 0.01, 0),
+])
+def test_cash_flow_pv(today, cf_date, rate, expected):
+    curve = ConstantDiscountRateCurve(today, rate)
     market = RatesTermStructure.of_single_curve(today, curve)
 
-    cf1 = CashFlow(100, datetime(2016, 1, 1))
-    assert present_value(cf1, market, as_of_date=today) == 100.0
-
-    curve = ConstantDiscountRateCurve(today, 0.01)
-    market = RatesTermStructure.of_single_curve(today, curve)
-
-    cf1 = CashFlow(100, datetime(2016, 1, 1))
-    assert round(present_value(cf1, market, as_of_date=today), 3) == 99.005
+    cf1 = CashFlow(100, cf_date)
+    assert round(present_value(cf1, market, as_of_date=today), 3) == expected
